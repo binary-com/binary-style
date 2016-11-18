bdfacd46a7c9b8483369c5b82c50e073584b01b0
negar <negar@binary.com>
2016-11-18 10:40:00 +0800

    add navigation to binary style

---
+++ style.js
@@ -0,0 +1,58 @@
+/*
+ * js code for binary style
+ */
+$(document).ready(function() {
+    initSections();
+
+    var listItem, $listItem, $optionItem;
+
+    function initVariables() {
+        listItem    = '.page-wrapper > .sidebar-left li';
+        $listItem   = $(listItem);
+        $optionItem = $('#mobile-page-selector');
+    }
+
+    function initSections() {
+        var currentPath = window.location.hash === '' ? '/' : window.location.hash;
+        initVariables();
+        populateOptionMenu();
+        displaySection(null, currentPath);
+        displaySection('mobile', currentPath);
+        $listItem.on('click', function() { displaySection(); });
+        $optionItem.on('change', function() { displaySection('mobile'); });
+    }
+
+    function populateOptionMenu() {
+        $listItem.each(function() {
+            $optionItem.append('<option value="' + $(this).find('a').attr('href') + '">' + $(this).text() + '</option>');
+        });
+    }
+
+    function selectListItemMenu(id) {
+        $listItem.removeClass('selected').find('a[href="' + id + '"]').parent().addClass('selected');
+    }
+
+    function selectOptionMenu(id) {
+        $optionItem.find('option').removeAttr('selected').end().find('option[value="' + id + '"]').attr('selected', 'selected');
+    }
+
+    function displaySection(mobileView, currentPath) {
+        var hashValue;
+        if (mobileView) {
+            hashValue = currentPath || $optionItem.val();
+            selectListItemMenu(hashValue);
+        } else {
+            hashValue = currentPath || $(listItem + '.selected a').attr('href');
+            selectOptionMenu(hashValue);
+        }
+        var classToShow = hashValue.split('#')[1];
+        if (hashValue === '/') {
+            history.replaceState("", document.title, window.location.pathname);
+        } else {
+            window.location.hash = hashValue;
+        }
+        if (!classToShow || $('.' + classToShow).length === 0) classToShow = 'introduction-section';
+        $('.f-row').hide();
+        $('.' + classToShow).css('display', 'flex');
+    }
+});
